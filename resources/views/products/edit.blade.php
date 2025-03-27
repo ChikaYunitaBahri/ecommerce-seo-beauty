2bde68795c94e35c4749ce92994a1a7422bf5710
wenynrf <h1101231044@student.untan.ac.id>
2025-03-27 21:51:39 +0700

    Memperbarui Isi File Product View edit.blade.php

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Edit Product</h2>
+    <form action="{{ route('products.update', $product->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" class="form-control" value="{{ $product->name }}" required>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" class="form-control" required>{{ $product->description }}</textarea>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" name="price" class="form-control" value="{{ $product->price }}" required>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-success">Update Product</button>
+    </form>
+</div>
+@endsection
